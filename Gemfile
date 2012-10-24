e635b4e977b4c2e87442bf898feea401c7d693fe
matiasow <matias@becloudy.com>
2012-10-24 17:45:52 -0300

    New web site merged

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
+source 'https://rubygems.org'
+
 gem 'sinatra'
+gem 'sass'
+gem 'slim'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require 'slim'
+require 'sass'
 
 module BeCloudy
   class App < Sinatra::Application
     get '/' do
-      "Hello, World!"
+      slim :index
+    end
+
+    get '/css/:name.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      sass(:"stylesheets/#{params[:name]}")
     end
   end
 end
